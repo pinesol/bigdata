e75319ae6d6b82782dcb393d808ed96fa2ac5971
pinesol <alpinesol@gmail.com>
2015-04-26 16:26:46 -0400

    Created script to make an SQL table from the data

---
+++ create_table.sql
@@ -0,0 +1,39 @@
+-- Create table for yellow and green cab info
+-- NOTE: You have to 'create' and 'use' a database beforehand. 
+-- e.g. 'create database taxi; use database taxi'.
+
+-- NOTE: make sure to change the name of 'testtable.csv'
+
+CREATE TABLE trips (
+    color VARCHAR(1),
+    zone VARCHAR(6),
+    pickup_neighborhood VARCHAR(32),
+    pickup_borough VARCHAR(9),
+    dropoff_neighborhood VARCHAR(32),
+    dropoff_borough VARCHAR(9),
+    pickup_date DATE,
+    pickup_hour SMALLINT,
+    pickup_longitude DECIMAL(15,10),
+    pickup_latitude DECIMAL(15,10),
+    dropoff_longitude DECIMAL(15,10),
+    dropoff_latitude DECIMAL(15,10),
+    passenger_count INTEGER,
+    trip_distance DECIMAL(12,4),
+    fare_amount DECIMAL(15,4),
+    surcharge DECIMAL(15,4),
+    mta_tax DECIMAL(15,4),
+    tip_amount DECIMAL(15,4),
+    tolls_amount DECIMAL(15,4),
+    total_amount DECIMAL(15,4),
+    num_trips INTEGER
+);
+
+CREATE INDEX color_index
+ON trips 
+(color, zone, pickup_borough, pickup_neighborhood, dropoff_neighborhood);
+
+
+LOAD DATA LOCAL INFILE 'testtable.csv' 
+INTO TABLE trips 
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n';
