1c08c38fa04322094feac4c6461024986724e7df
im965 <israelmalkin@nyu.edu>
2015-05-05 19:23:07 -0400

    altered sql maketable file to include new features

--- create_table.sql
+++ create_table.sql
@@ -6,11 +6,14 @@
 
 CREATE TABLE trips (
     color VARCHAR(1),
-    zone VARCHAR(6),
     pickup_neighborhood VARCHAR(32),
     pickup_borough VARCHAR(9),
     dropoff_neighborhood VARCHAR(32),
     dropoff_borough VARCHAR(9),
+    pickup_zone VARCHAR(6),
+    dropoff_zone VARCHAR(6),
+    pickup_border VARCHAR(6),
+    dropoff_border VARCHAR(6),
     pickup_date DATE,
     pickup_hour SMALLINT,
     pickup_longitude DECIMAL(15,10),
